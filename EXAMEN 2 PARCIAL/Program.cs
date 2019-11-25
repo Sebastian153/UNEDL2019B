3bd1f3a269936b35c70f1b1520810572efaf7105
Sebastian153 <54988932+Sebastian153@users.noreply.github.com>
2019-11-25 11:02:27 -0600

    Add files via upload

---
+++ EXAMEN 2 PARCIAL/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace pilaecuaciones
+{
+    using System.Collections;
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Ingresar la ecuacion para ver si es congruente o incongruente:  ");
+
+            String IngresarEcuacion = Console.ReadLine();
+
+
+
+            Stack ecuacionEvaluar = new Stack();
+
+
+
+            for (int i = 0; i < IngresarEcuacion.Length; ++i)
+
+            {
+
+                if ((IngresarEcuacion[i] == '(') || (IngresarEcuacion[i] == '{') || (IngresarEcuacion[i] == '['))
+
+                {
+
+                    ecuacionEvaluar.Push(IngresarEcuacion[i]);
+
+                }
+
+                else if (ecuacionEvaluar.Count > 0)
+
+                {
+
+                    switch (IngresarEcuacion[i])
+
+                    {
+
+                        case ']':
+                            ecuacionEvaluar.Pop();
+
+                            break;
+
+                        case '}':
+                            ecuacionEvaluar.Pop();
+
+                            break;
+
+                        case ')':
+                            ecuacionEvaluar.Pop();
+
+                            break;
+
+                    }
+
+                }
+
+            }
+
+            if (ecuacionEvaluar.Count == 0)
+
+            {
+
+                Console.WriteLine("LA ECUACION ESTA CORRECTA");
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine("LA ECUACION NO ES CORRECTA");
+
+            }
+
+        }
+
+    }
+
+}
